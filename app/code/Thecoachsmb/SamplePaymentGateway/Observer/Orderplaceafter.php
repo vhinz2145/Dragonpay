f236339cf0cbaf5e2a679b3ca652d3c1123fc5af
vhinz2145 <130061358+vhinz2145@users.noreply.github.com>
2023-04-06 21:14:08 +0800

    paypal v2

--- app/code/Thecoachsmb/SamplePaymentGateway/Observer/Orderplaceafter.php
+++ app/code/Thecoachsmb/SamplePaymentGateway/Observer/Orderplaceafter.php
@@ -35,6 +35,7 @@ class Orderplaceafter implements ObserverInterface
             'ccy' => $order->getCurrencyCode(),
             'description' => 'My order description.',
             'email' => $address->getEmail()
+            //7aa0e0f
         );
         }
             $this->_redirect($url,$params);
